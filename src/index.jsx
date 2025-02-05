bd262aac105d3b4be3cb81fbcefdf685c8034cf8
Agil Fikriawan <agilfikriawan2803@gmail.com>
2025-02-05 13:58:12 +0800

    add routes

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,13 @@
+// eslint-disable-next-line no-unused-vars
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import ContactApp from './components/ContactApp';
 import './styles/style.css';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = createRoot(document.getElementById('root'));
-root.render(<ContactApp />);
+root.render(
+  <BrowserRouter>
+    <ContactApp />
+  </BrowserRouter>
+);
